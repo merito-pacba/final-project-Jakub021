a9c1b5011d1f94462f973077e0aea6c2e1c13180
Jakub021 <jakubwloch21@gmail.com>
2024-02-08 02:51:24 +0100

    test

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-"""
-This app shows how to perform CRUD (Create, Retrieve, Update, Delete) 
-operations in Flask web application connected to MS SQL Server.
-"""
-
 import os
 import pyodbc
 from flask import Flask,render_template,request,url_for,redirect
@@ -14,69 +9,52 @@ conn_str = os.environ.get("CONN_STR")
 port = os.environ.get("PORT", "5000")
 
 app = Flask(__name__)
-
 conn = pyodbc.connect(conn_str)
 
 
 @app.route("/")
 def index():
-    """Start page of the demo application"""
     return render_template("index.html")
 
-
 @app.route("/query1")
-def query():
-    """Performs simple query to DB"""
-    page = "<h1>Simple Query</h1>\n<ol>\n"
+def query1():
     with conn.cursor() as cursor:
         cursor.execute("SELECT TOP (10) * FROM [SalesLT].[Address]")
-        row = cursor.fetchone()
-        while row:
-            page += "<li>" +  str(row[1]) + " " + str(row[3]) + "</li>\n"
-            row = cursor.fetchone()
-    page += "</ol>"
-    return page
+        rows = cursor.fetchall()
+    return render_template("query1result.html", rows=rows)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 @app.route("/query2")
 def query2():
-    """Performs better query to DB"""
-    page = "<h1>Better Query</h1>\n<table>\n"
-    page += "<tr><td>Street</td><td>City</td></tr>"
     with conn.cursor() as cursor:
-        cursor.execute("""SELECT TOP (10) [AddressID],
-                            [AddressLine1],
-                            [City],
-                            [StateProvince],
-                            [CountryRegion],
-                            [PostalCode]
-                        FROM [SalesLT].[Address]""")
-        row = cursor.fetchone()
-        while row:
-            page += "<tr><td>" +  row.AddressLine1 + "</td><td>" + row.City + "</td></tr>\n"
-            row = cursor.fetchone()
-    page += "</table>"
-    return page
+        cursor.execute("""SELECT [AddressID],
+                                [AddressLine1],
+                                [City]
+                            FROM [SalesLT].[Address]
+                            ORDER BY [AddressID] DESC""")
+        rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        return render_template("query2result.html", rows=rows, columns=columns)
 
 
 @app.route("/query3")
 def query3():
-    """Performs query to DB using template to display it"""
     with conn.cursor() as cursor:
-        # As new addresses are added in the end, ORDER BY ... DESC reverses order
-        # of rows, so TOP(10) displays 10 LAST addresses.
-        cursor.execute("""SELECT TOP (10) [AddressID],
-                            [AddressLine1],
-                            [City]
-                        FROM [SalesLT].[Address]
-                        ORDER BY [AddressID] DESC""")
-        all_rows = cursor.fetchall()
-        return render_template("query3result.html", rows = all_rows)
+        cursor.execute("""SELECT [AddressID],
+                                [AddressLine1],
+                                [City]
+                            FROM [SalesLT].[Address]
+                            ORDER BY [AddressID] DESC""")
+        rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        return render_template("query3result.html", rows=rows, columns=columns)
 
 
-@app.route("/edit/<int:address_id>", methods=['POST','GET'])
+@app.route("/edit/<int:address_id>", methods=['POST', 'GET'])
 def edit(address_id):
-    """Performs query to DB but display using template"""
     if request.method == 'GET':
         with conn.cursor() as cursor:
             cursor.execute(f"""SELECT [AddressID],
@@ -93,39 +71,43 @@ def edit(address_id):
         with conn.cursor() as cursor:
             cursor.execute("""UPDATE [SalesLT].[Address]
                 SET [AddressLine1] = ?, [City] = ?
-                WHERE [AddressID] = ?""", 
+                WHERE [AddressID] = ?""",
                 (request.form['addressline1'], request.form['city'], address_id))
             conn.commit()
         return redirect(url_for('query3'))
 
 
 @app.route("/delete/<int:address_id>")
-def delete(address_id):
-    """Delete one item from the table"""
+def delete_address(address_id):
     with conn.cursor() as cursor:
-        cursor.execute("""DELETE FROM [SalesLT].[Address]
-            WHERE [AddressID] = ?""", 
-            (address_id))
+        cursor.execute("DELETE FROM [SalesLT].[Address] WHERE [AddressID] = ?", (address_id,))
         conn.commit()
-    return redirect(url_for('query3'))
+    return redirect(url_for('query2'))
 
 
 @app.route("/insert", methods=['POST','GET'])
 def insert():
-    """Inserts a new item into table"""
     if request.method == 'GET':
         return render_template("insert.html")
     else:
         with conn.cursor() as cursor:
-            # Some of fields required by Address table are not present 
-            # in the form, so the default values (eg. for 'province' 
-            # for StateProvince) are provided
             cursor.execute("""INSERT INTO [SalesLT].[Address] 
                 (AddressLine1, City, StateProvince, CountryRegion, PostalCode, ModifiedDate) 
-                VALUES (?, ?, 'province', 'region', '0000', '2006-07-01 00:00:00.000' )""", 
+                VALUES (?, ?, 'province', 'region', '0000', '2006-07-01 00:00:00.000' )""",
                 (request.form['addressline1'], request.form['city']))
             conn.commit()
         return redirect(url_for('query3'))
+
+@app.route("/search", methods=['GET'])
+def search():
+    query = request.args.get('query')
+    with conn.cursor() as cursor:
+        cursor.execute("""SELECT [AddressID], [AddressLine1], [City]
+                          FROM [SalesLT].[Address]
+                          WHERE [AddressLine1] LIKE ? OR [City] LIKE ?""",
+                       ('%' + query + '%', '%' + query + '%'))
+        rows = cursor.fetchall()
+    return render_template("query1result.html", rows=rows)
 
 
 if __name__ == "__main__":
